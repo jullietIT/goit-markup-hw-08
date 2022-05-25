57c68b253b9b1cbb0bf9189a48f24b8daaaa7255
Juliya Khanenko <jullietnm@gmail.com>
2022-05-25 21:17:48 +0300

    мenu

--- js/mobile-menu.js
+++ js/mobile-menu.js
@@ -1,14 +1,14 @@
 (() => {
-    const menuBtnRef = document.querySelector("[data-menu-button]");
-    const mobileMenuRef = document.querySelector("[data-menu]");
-    const body = document.querySelector('[data-body]');
-    menuBtnRef.addEventListener("click", () => {
+  const menuBtnRef = document.querySelector("[data-menu-button]");
+  const mobileMenuRef = document.querySelector("[data-menu]");
+
+  menuBtnRef.addEventListener("click", () => {
       const expanded =
-        menuBtnRef.getAttribute("aria-expanded") === "true" || false;
+          menuBtnRef.getAttribute("aria-expanded") === "true" || false;
+      
       menuBtnRef.classList.toggle("is-open");
       menuBtnRef.setAttribute("aria-expanded", !expanded);
+
       mobileMenuRef.classList.toggle("is-open");
-      body.classList.toggle('body--scroll-hidden');
-		body.classList.toggle('body--scroll-hidden::-webkit-scrollbar');
-    });
-  })();
+  })
+})();
